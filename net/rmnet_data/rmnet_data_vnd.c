40764104d355126752bf39b22d6c1fe026fdc044
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2017-02-23 18:24:42 -0700

    net: rmnet_data: Add support to configure custom device name
    rmnet_data assigns device name by the order they are created.
    This causes problems which multiple processes are trying to
    create devices and leads to random device names.
    
    Assign device name as specified by user.
    
    [mikeioannina]: Backport to 3.10
    
    CRs-Fixed: 2018785
    Change-Id: Iab8e053c6ccacbeedaa7763e760d0c12e756b5d0
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>

--- net/rmnet_data/rmnet_data_vnd.c
+++ net/rmnet_data/rmnet_data_vnd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -547,7 +547,7 @@ int rmnet_vnd_init(void)
  *      - return code of register_netdevice() on other errors
  */
 int rmnet_vnd_create_dev(int id, struct net_device **new_device,
-			 const char *prefix)
+			 const char *prefix, int use_name)
 {
 	struct net_device *dev;
 	char dev_prefix[IFNAMSIZ];
@@ -558,12 +558,15 @@ int rmnet_vnd_create_dev(int id, struct net_device **new_device,
 		return -EINVAL;
 	}
 
-	if (!prefix)
+	if (!prefix && !use_name)
 		p = scnprintf(dev_prefix, IFNAMSIZ, "%s%%d",
 			  RMNET_DATA_DEV_NAME_STR);
+	else if (prefix && use_name)
+		p = scnprintf(dev_prefix, IFNAMSIZ, "%s", prefix);
+	else if (prefix && !use_name)
+		p = scnprintf(dev_prefix, IFNAMSIZ, "%s%%d", prefix);
 	else
-		p = scnprintf(dev_prefix, IFNAMSIZ, "%s%%d",
-			  prefix);
+		return RMNET_CONFIG_BAD_ARGUMENTS;
 	if (p >= (IFNAMSIZ-1)) {
 		LOGE("Specified prefix (%d) longer than IFNAMSIZ", p);
 		return -EINVAL;
